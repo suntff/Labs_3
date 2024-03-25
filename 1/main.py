3531ca97179a8ba486638a307e60d499d1ee8022
suntff <145331931+suntff@users.noreply.github.com>
2024-03-25 11:37:21 +0200

    Update main.py

--- 1/main.py
+++ 1/main.py
@@ -1,14 +1,16 @@
-a = 'baafhasrrrrrddww'+" "
-#str(input())
-i = 1
+in_str = str(input())+' '
+out_str = ''
 c = 1
-while i<len(a):
-    if a[i] == a[i - 1]:
-        c += 1
-    else:
-        if c != 1:
-            a = a[:i-c+1] + str(c) + a[i:]
-            i = len(a[:i-c] + str(c))-1
-            c = 1
-    i+=1
-print(a)
+
+for i in range(len(in_str)):
+    if i > 0:
+        if in_str[i] == in_str[i - 1]:
+            c += 1
+        else:
+            if c != 1:
+                out_str += in_str[i - c] + str(c)
+                c = 1
+            else:
+                out_str += in_str[i - 1]
+
+print(out_str)
